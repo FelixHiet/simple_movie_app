422f595cda5e3a2fa4934f884acb2e6a7b57203d
Felix Hietaranta <49163212+FelixHiet@users.noreply.github.com>
2022-09-14 11:50:22 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,124 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import SearchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
 
-function App() {
+const API_URL = "http://www.omdbapi.com?apikey=aee29290"; //uses OMDb API key
+
+const movie1 = {
+  Title: "Spiderman",
+  Year: "1990",
+  imdbID: "tt0100669",
+  Type: "movie",
+  Poster: "N/A",
+};
+
+const App = () => {
+  const [movies, setMovies] = useState([]); //empty array
+  const [searchTerm, setSearchTerm] = useState(""); //empty string
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`); //this will call the API
+    const data = await response.json();
+
+    setMovies(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Avengers");
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Simple Movie App</h1>
+
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">No movies found</div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
+
+/*
+
+{movie1.Poster !== "N/A" ? movie1.Poster : "https://via.placeholder.com/400"}
+//if poster is not equal to N/A then we can render movie1.poster, but if there is no image then we can render placeholder image
+
+*/
+
+/*
+
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import SearchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
+
+const API_URL = "http://www.omdbapi.com?apikey=aee29290"; //uses OMDb API key
+
+const movie1 = {
+  Title: "Spiderman",
+  Year: "1990",
+  imdbID: "tt0100669",
+  Type: "movie",
+  Poster: "N/A",
+};
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`); //this will call the API
+    const data = await response.json();
+
+    setMovies(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Avengers");
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>Simple Movie App</h1>
+      <div className="search">
+        <input placeholder="Search for movies" value={""} onChange={() => {}} />
+        <img src={SearchIcon} alt="search" onClick={() => {}} />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">No movies found</div>
+      )}
+    </div>
+  );
+};
+
+export default App;
+
+*/
---
+++ src/MovieCard.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const MovieCard = ({ movie }) => {
+  return (
+    <div className="movie">
+      <div>
+        <p>{movie.Year}</p>
+      </div>
+
+      <div>
+        <img //if poster is not equal to N/A then we can render movie1.poster, but if there is no image then we can render placeholder image
+          src={
+            movie.Poster !== "N/A"
+              ? movie.Poster
+              : "https://via.placeholder.com/400"
+          }
+          alt={movie.Title}
+        />
+      </div>
+
+      <div>
+        <span>{movie.Type}</span>
+        <h3>{movie.Title}</h3>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
